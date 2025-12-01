7c15491bc5d1ecf8697950e11227c32e30039603
mezavg <tosha.linberg2@gmail.com>
2025-12-01 16:45:05 +0200

    2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,19 +5,22 @@
  */
 function calculateRentalCost(days) {
   // write code here
-  if (days === 7) {
-    return 230;
+
+  const DAILY_RATE = 40;
+  const DISCOUNT_3_DAYS = 20;
+  const DISCOUNT_7_DAYS = 50;
+
+  let totalCost = days * DAILY_RATE;
+
+  if (days >= 7) {
+    totalCost -= DISCOUNT_7_DAYS;
+  } else if (days >= 3) {
+    totalCost -= DISCOUNT_3_DAYS;
   }
 
-  if (days === 6) {
-    return 220;
-  }
-
-  if (days === 3) {
-    return 100;
-  }
-
-  return 80;
+  return totalCost;
 }
+
+module.exports = calculateRentalCost;
 
 module.exports = calculateRentalCost;
